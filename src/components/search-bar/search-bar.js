cfc72cb12bdf1ad3b3e931c5fe1291664a504bc8
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-10 11:37:18 -0700

    Implement query service

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -5,6 +5,9 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 
+// Services
+import queryService from "../../services";
+
 // CSS styles
 import "./search-bar.css";
 
@@ -12,7 +15,7 @@ class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: "",
+            query: "",
             hover: false
         }
         this.onFormSubmit = this.onFormSubmit.bind(this);
@@ -23,7 +26,21 @@ class SearchBar extends React.Component {
     // TODO: Implement
     onFormSubmit(e) {
         e.preventDefault();
-        this.props.onSearch(this.state.search);
+        
+        const { query } = this.state;
+        const { onSearch } = this.props;
+
+        // A query should not take place if the search is
+        // an empty string or whitespace.
+        // if (!search || search.trim().length === 0) {
+        //     this.props.onSearch(null);
+        //     return;
+        // }
+
+        // const queryTerms = search.trim().split(" ");
+        // const lowercaseQueryTerms = queryTerms.map(term => term.toLocaleLowerCase());
+        
+        onSearch( queryService.prepareQuery(query) );
     }
 
     onChange(e) {
@@ -50,10 +67,10 @@ class SearchBar extends React.Component {
                         <Form.Control
                             className="w-100 border-0"
                             type="text"
-                            name="search"
+                            name="query"
                             placeholder="Search away..."
                             onChange={this.onChange}
-                            value={this.state.search}
+                            value={this.state.query}
                         >
                         </Form.Control>
                     </Form.Group>
