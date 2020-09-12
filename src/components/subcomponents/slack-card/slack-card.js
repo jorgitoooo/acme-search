8a6529619ad97ceea92dc33df751a2df17ef1527
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-12 13:21:48 -0700

    Modify to include card info styled component

--- src/components/subcomponents/slack-card/slack-card.js
+++ src/components/subcomponents/slack-card/slack-card.js
@@ -5,7 +5,7 @@ import DateInfo from "../date-info";
 
 // Styled components
 import Card from "react-bootstrap/Card";
-import Image from "react-bootstrap/Image";
+import CardInfo from "../../styled-components/card-info";
 
 // SVG icons
 import slackHashIcon from "../../../assets/slack-hash.svg";
@@ -17,25 +17,23 @@ function SlackCard( { channel, author, message, createdAt, ...props } ) {
             <Card.Body>
                 <h3 className="font-weight-bold">{author}</h3>
                 { channel && (
-                    <div>
-                        <div className="d-flex justify-content-center align-items-center mb-1">
-                            <Image src={slackHashIcon} width={30}/>
-                            <h5 className="font-weight-bold mb-0 ml-1">Channel</h5>
-                        </div>
-                        <p className="text-muted">{channel}</p>
-                    </div>
+                    <CardInfo 
+                        iconSrc={slackHashIcon}
+                        heading={"Channel"}
+                        content={channel}
+                    />
                 )}
                 { message && (
-                    <div>
-                        <div className="d-flex justify-content-center align-items-center mb-1">
-                            <Image src={messageCircleIcon} width={30}/>
-                            <h5 className="font-weight-bold mb-0 ml-1">Message</h5>
-                        </div>
-                        <p className="text-muted mb-0">{message}</p>
-                    </div>
+                    <CardInfo
+                        iconSrc={messageCircleIcon}
+                        heading={"Message"}
+                        content={message}
+                    />
                 )}
             </Card.Body>
-            <Card.Footer className="bg-white">{createdAt && <DateInfo title={"Sent"} date={createdAt} />}</Card.Footer>
+            <Card.Footer className="bg-white">
+                { createdAt && <DateInfo title={"Sent"} date={createdAt} /> }
+            </Card.Footer>
         </Card>
     );
 }
