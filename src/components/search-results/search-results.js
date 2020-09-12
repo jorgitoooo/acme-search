6dcee3539b71c53335131f2bc30067f9209a71cc
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-12 13:24:45 -0700

    Modify to include tweet list component

--- src/components/search-results/search-results.js
+++ src/components/search-results/search-results.js
@@ -3,6 +3,7 @@ import CalendarList from "../calendar-list";
 import ContactList from "../contact-list";
 import DropboxList from "../dropbox-list";
 import SlackList from "../slack-list";
+import TweetList from "../tweet-list";
 
 // Services
 import services from "../../services";
@@ -14,39 +15,35 @@ function SearchResults({ query, ...props }) {
     const [results, setResults] = useState(null);
 
     useEffect(() => {
-        if (query || Object.is({}, query)) {
+        if (query) {
             const calendarFiltered = services.calendar.getMatching(query);
             const contactsFiltered = services.contact.getMatching(query);
             const dropboxFiltered = services.dropbox.getMatching(query);
             const slackFiltered = services.slack.getMatching(query);
-            const tweetFiltered = services.tweet.getMatching(query);
+            const tweetsFiltered = services.tweet.getMatching(query);
 
             setResults({
                 calendar: calendarFiltered,
                 contacts: contactsFiltered,
                 dropbox: dropboxFiltered,
                 slack: slackFiltered,
-                tweet: tweetFiltered,
+                tweets: tweetsFiltered,
             });
         }
     }, [query]);
 
     if (results) {
-        // DEV
-        const tweetList = results.tweet.map((res, idx) => <li key={idx}>tweet: {res.user}</li>);
         return (
             <section className="results mx-auto text-center" {...props}>
                 <CalendarList calendar={results.calendar} />
                 <ContactList contacts={results.contacts} />
                 <DropboxList documents={results.dropbox} />
                 <SlackList messages={results.slack} />
-
-                { tweetList }
+                <TweetList tweets={results.tweets} />
             </section>
         );
     } else {
         return null;
-        // return <p {...props} >No results...</p>
     }
 }
 
