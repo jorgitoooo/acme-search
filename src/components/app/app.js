2558030b12261fd35fad27bb2b800ff4fb8cea70
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-11 20:25:44 -0700

    Add padding to main tag

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import SearchResults from "../search-results";
 import SearchBar from "../search-bar";
 import SearchCard from "../search-card";
+import SearchResults from "../search-results";
+
+// CSS styles
+import "./app.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ""
+      query: null
     }
     this.onSearch = this.onSearch.bind(this);
-
-    console.log("constructor");
   }
 
   onSearch(query) {
     this.setState({ query });
   }
 
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
   render() {
     return (
-      <main className="text-center p-3">
+      <main className="p-3">
         <SearchCard>
           <SearchBar onSearch={this.onSearch} />
           <SearchResults query={this.state.query} />
