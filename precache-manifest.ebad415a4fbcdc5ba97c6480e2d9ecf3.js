1cb8af8e3d55a9e8e5fdfa37f7d4e41c9327e409
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-13 20:14:07 -0700

    Updates

---
+++ precache-manifest.ebad415a4fbcdc5ba97c6480e2d9ecf3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131c12b38eb75c6d05c36e191bbfc581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d03968a806061e1dc44a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2a8b3c38124338703fe1",
+    "url": "/static/css/main.782e8f2e.chunk.css"
+  },
+  {
+    "revision": "d03968a806061e1dc44a",
+    "url": "/static/js/2.184dc2c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.184dc2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8b3c38124338703fe1",
+    "url": "/static/js/main.b4a59a35.chunk.js"
+  },
+  {
+    "revision": "c5dd69aee1f292111dbd",
+    "url": "/static/js/runtime-main.6702424c.js"
+  },
+  {
+    "revision": "6367b939805cd1f0be3a789746f279fb",
+    "url": "/static/media/email.6367b939.svg"
+  },
+  {
+    "revision": "1ff296c57d0e3bf340a7b3ede138e4c4",
+    "url": "/static/media/file.1ff296c5.svg"
+  },
+  {
+    "revision": "d7f243001b23a61decc58f5bf5809d03",
+    "url": "/static/media/message-circle.d7f24300.svg"
+  },
+  {
+    "revision": "54b44710de3e472d816aab741f1cbe95",
+    "url": "/static/media/people-outline.54b44710.svg"
+  },
+  {
+    "revision": "9648114a7df6e3a3a2401169d9fa22ef",
+    "url": "/static/media/phone.9648114a.svg"
+  },
+  {
+    "revision": "99c580ba44df7570c2f43190d011f4c1",
+    "url": "/static/media/slack-hash.99c580ba.svg"
+  },
+  {
+    "revision": "a1aa34d690b514c322ce1aa754887940",
+    "url": "/static/media/twitter.a1aa34d6.svg"
+  }
+]);
