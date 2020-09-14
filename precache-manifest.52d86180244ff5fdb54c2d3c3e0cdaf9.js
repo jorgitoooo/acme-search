9a099af6f8cb190b78c12ab18e52ef39b8f42479
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-13 20:40:06 -0700

    Updates

---
+++ precache-manifest.52d86180244ff5fdb54c2d3c3e0cdaf9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce44989d6282c2cbd5ce2555dc16e24",
+    "url": "/neeva-assessment/index.html"
+  },
+  {
+    "revision": "d03968a806061e1dc44a",
+    "url": "/neeva-assessment/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b5f2b82bf9674b960f6f",
+    "url": "/neeva-assessment/static/css/main.782e8f2e.chunk.css"
+  },
+  {
+    "revision": "d03968a806061e1dc44a",
+    "url": "/neeva-assessment/static/js/2.184dc2c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/neeva-assessment/static/js/2.184dc2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f2b82bf9674b960f6f",
+    "url": "/neeva-assessment/static/js/main.87f6b2df.chunk.js"
+  },
+  {
+    "revision": "bf7646898a373ac56a7c",
+    "url": "/neeva-assessment/static/js/runtime-main.358bde67.js"
+  },
+  {
+    "revision": "6367b939805cd1f0be3a789746f279fb",
+    "url": "/neeva-assessment/static/media/email.6367b939.svg"
+  },
+  {
+    "revision": "1ff296c57d0e3bf340a7b3ede138e4c4",
+    "url": "/neeva-assessment/static/media/file.1ff296c5.svg"
+  },
+  {
+    "revision": "d7f243001b23a61decc58f5bf5809d03",
+    "url": "/neeva-assessment/static/media/message-circle.d7f24300.svg"
+  },
+  {
+    "revision": "54b44710de3e472d816aab741f1cbe95",
+    "url": "/neeva-assessment/static/media/people-outline.54b44710.svg"
+  },
+  {
+    "revision": "9648114a7df6e3a3a2401169d9fa22ef",
+    "url": "/neeva-assessment/static/media/phone.9648114a.svg"
+  },
+  {
+    "revision": "99c580ba44df7570c2f43190d011f4c1",
+    "url": "/neeva-assessment/static/media/slack-hash.99c580ba.svg"
+  },
+  {
+    "revision": "a1aa34d690b514c322ce1aa754887940",
+    "url": "/neeva-assessment/static/media/twitter.a1aa34d6.svg"
+  }
+]);
