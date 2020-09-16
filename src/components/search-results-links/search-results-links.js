926fd45583c3175371ca1cf7973ef44ec3a1996e
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-15 20:07:07 -0700

    Track search result link clicks

--- src/components/search-results-links/search-results-links.js
+++ src/components/search-results-links/search-results-links.js
@@ -1,18 +1,27 @@
 import React from "react";
 
+// Utilities
+import utils from "../../utils";
+
 // CSS styles
 import "./search-results-links.css";
 
 function SearchResultsLinks({ results }) {
+    function trackClick(e) {
+        utils.analytics.clickEvent(`user clicked search result link ${e.target.hash}`);
+    }
+
     if (results) {
       return(
         <div className="d-flex flex-wrap justify-content-center">
             {Object.keys(results).map(key => {
                 if (results[key].length > 0) {
                   return (
-                    <a 
+                    <a
+                      key={key}
                       href={`#${key}`}
                       className="m-1 py-1 px-2 badge badge-pill badge-secondary shadow badge-md"
+                      onClick={trackClick}
                     >
                       {key}
                     </a>
