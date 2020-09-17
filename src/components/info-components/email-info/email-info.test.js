8286c62c0050fe20a6f69d2745d6974cb8bbf150
Jorge L. Garcia <me@jorgegarcia.co>
2020-09-16 19:48:59 -0700

    Remove all unnecessary test files

--- src/components/info-components/email-info/email-info.test.js
+++ src/components/info-components/email-info/email-info.test.js
@@ -16,35 +16,5 @@ describe("EmailInfo: Test visual components", () => {
 
     it("renders correctly", () => {
         expect(toJson(wrapper)).toMatchSnapshot();
-    })
-
-    // it("displays correct title and date", () => {
-    //     expect(wrapper.find("h5").first().text()).toEqual(props.title);
-    //     expect(wrapper.find("p").first().text()).toEqual(props.date);
-    // });
+    });
 });
-
-// describe("EmailInfo: Test prop type validation", () => {
-//     let consoleErrorSpy;
-//     beforeEach(() => {
-//         console.error = jest.fn()
-//         consoleErrorSpy = jest.spyOn(console, "error");
-//     });
-
-//     afterEach(() => {
-//         // It's best practice to clear spied function after every test run.
-//         // https://stackoverflow.com/questions/43245040/
-//         //   using-jest-to-spy-on-method-call-in-componentdidmount/43284406#43284406
-//         consoleErrorSpy.mockClear();
-//     });
-    
-//     it("throws an error when title prop is missing", () => {
-//         mount(<EmailInfo date={props.date} />);
-//         expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
-//     });
-  
-//     it("throws an error when date prop is missing", () => {
-//         mount(<EmailInfo title={props.title} />);
-//         expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
-//     });
-// });
